89884d863465356d5f6eb687b81435daf3fe427e
md ranit rubbyat sultan <mdranitrubbyatsultan@gmail.com>
2023-12-05 00:04:14 +0600

    make some small update

--- src/components/shared/Header/Header.jsx
+++ src/components/shared/Header/Header.jsx
@@ -6,61 +6,89 @@ import { AuthContext } from "../../../Providers/AuthProvider";
 import { toast } from "react-hot-toast";
 
 const Header = () => {
-    const { user, logOut } = useContext(AuthContext);
+  const { user, logOut } = useContext(AuthContext);
 
-    const handelSignOut = () => {
-        logOut()
-            .then(() => toast.success("LogOut Successful"))
-            .catch(er => toast.error(er.message))
-    }
+  const handelSignOut = () => {
+    logOut()
+      .then(() => toast.success("LogOut Successful"))
+      .catch((er) => toast.error(er.message));
+  };
 
-    const links = <>
-        <li><NavLink className={``} to={'/'}>Home</NavLink></li>
-        {
-            user ?
-                <>
-                    <li><NavLink to={'/upcoming'}>Upcoming</NavLink></li>
-                    <li><NavLink to={'/query'}>Query</NavLink></li>
-                    <li onClick={handelSignOut}><Link to={'/'}>Sign Out</Link></li>
-                </>
-                :
-                <li><NavLink to={'/login'}>Login</NavLink></li>
-        }
+  const links = (
+    <>
+      <li>
+        <NavLink className={``} to={"/"}>
+          Home
+        </NavLink>
+      </li>
+      {user ? (
+        <>
+          <li>
+            <NavLink to={"/upcoming"}>Upcoming</NavLink>
+          </li>
+          <li onClick={handelSignOut}>
+            <Link to={"/"}>Sign Out</Link>
+          </li>
+        </>
+      ) : (
+        <li>
+          <NavLink to={"/login"}>Login</NavLink>
+        </li>
+      )}
     </>
+  );
 
-    return (
-        <nav className="bg-black">
-            <div className="navbar text-white container mx-auto">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                        </label>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-50 p-2 shadow bg-black font-bold rounded-box w-52">
-                            {links}
-                        </ul>
-                    </div>
-                    <div className="normal-case text-xl whitespace-nowrap flex items-center gap-1">
-                        <img className="w-8 h-8" src={`${logo}`} alt="logo" />
-                        <span>usic Mode</span>
-                    </div>
-                </div>
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1 font-bold">
-                        {links}
-                    </ul>
-                </div>
-                <div className="navbar-end">
-                    <h4 className="text-md hidden md:block">{user?.displayName || ''}</h4>
-                    <label title={`${user?.displayName || ''}`} tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                        <div className="w-10 rounded-full">
-                            <img src={`${user?.photoURL || profileIcon}`} />
-                        </div>
-                    </label>
-                </div>
+  return (
+    <nav className="bg-black">
+      <div className="navbar text-white container mx-auto">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-50 p-2 shadow bg-black font-bold rounded-box w-52"
+            >
+              {links}
+            </ul>
+          </div>
+          <div className="normal-case text-xl whitespace-nowrap flex items-center gap-1">
+            <img className="w-8 h-8" src={`${logo}`} alt="logo" />
+            <span>usic Mode</span>
+          </div>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 font-bold">{links}</ul>
+        </div>
+        <div className="navbar-end">
+          <h4 className="text-md hidden md:block">{user?.displayName || ""}</h4>
+          <label
+            title={`${user?.displayName || ""}`}
+            tabIndex={0}
+            className="btn btn-ghost btn-circle avatar"
+          >
+            <div className="w-10 rounded-full">
+              <img src={`${user?.photoURL || profileIcon}`} />
             </div>
-        </nav>
-    );
+          </label>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
--- src/routes/router/router.jsx
+++ src/routes/router/router.jsx
@@ -7,50 +7,72 @@ import PrivetRoute from "../privetRoute/PrivetRoute";
 const Error = lazy(() => import("../../components/shared/Error/Error"));
 const Login = lazy(() => import("../../pages/Login/Login"));
 const Register = lazy(() => import("../../pages/Register/Register"));
-const UpcomingEvent = lazy(() => import("../../pages/UpcomingEvent/UpcomingEvent"));
-const Query = lazy(() => import("../../pages/QueryPage/Query"));
+const UpcomingEvent = lazy(() =>
+  import("../../pages/UpcomingEvent/UpcomingEvent")
+);
 // import ServiceDetails from "../../pages/ServiceDetails/ServiceDetails";
-const ServiceDetails = lazy(() => import("../../pages/ServiceDetails/ServiceDetails"));
-
+const ServiceDetails = lazy(() =>
+  import("../../pages/ServiceDetails/ServiceDetails")
+);
 
 export const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Root />,
-        errorElement: <Suspense fallback={<Spinner />}><Error /></Suspense>,
-        children: [
-            {
-                path: '/',
-                element: <Home />,
-                loader: async () => await fetch('/fakeData.json'),
-            },
-            {
-                path: '/service/:id',
-                element: <Suspense fallback={<Spinner />}><PrivetRoute><ServiceDetails /></PrivetRoute></Suspense>,
-                loader: async ({ params }) => {
-                    const res = await fetch('/fakeData.json');
-                    const data = await res.json();
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: (
+      <Suspense fallback={<Spinner />}>
+        <Error />
+      </Suspense>
+    ),
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+        loader: async () => await fetch("/fakeData.json"),
+      },
+      {
+        path: "/service/:id",
+        element: (
+          <Suspense fallback={<Spinner />}>
+            <PrivetRoute>
+              <ServiceDetails />
+            </PrivetRoute>
+          </Suspense>
+        ),
+        loader: async ({ params }) => {
+          const res = await fetch("/fakeData.json");
+          const data = await res.json();
 
-                    const singleData = data.find(single => single?.id === params.id)
-                    return singleData;
-                }
-            },
-            {
-                path: '/upcoming',
-                element: <Suspense fallback={<Spinner />}><PrivetRoute><UpcomingEvent /></PrivetRoute></Suspense>,
-            },
-            {
-                path: '/query',
-                element: <Suspense fallback={<Spinner />}><PrivetRoute><Query /></PrivetRoute></Suspense>,
-            },
-            {
-                path: '/login',
-                element: <Suspense fallback={<Spinner />}><Login /></Suspense>,
-            },
-            {
-                path: '/register',
-                element: <Suspense fallback={<Spinner />}><Register /></Suspense>,
-            },
-        ],
-    },
-])
+          const singleData = data.find((single) => single?.id === params.id);
+          return singleData;
+        },
+      },
+      {
+        path: "/upcoming",
+        element: (
+          <Suspense fallback={<Spinner />}>
+            <PrivetRoute>
+              <UpcomingEvent />
+            </PrivetRoute>
+          </Suspense>
+        ),
+      },
+      {
+        path: "/login",
+        element: (
+          <Suspense fallback={<Spinner />}>
+            <Login />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/register",
+        element: (
+          <Suspense fallback={<Spinner />}>
+            <Register />
+          </Suspense>
+        ),
+      },
+    ],
+  },
+]);
